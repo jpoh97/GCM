9494ebbd7bb810412bbd013c7af95d99062ac538
jpoh97 <jpoh97@gmail.com>
2016-05-19 14:03:01 -0500

    first commit

---
+++ GCM/Downstream/src/main/java/co/edu/udea/compumovil/gcm/Util.java
@@ -0,0 +1,16 @@
+package co.edu.udea.compumovil.gcm;
+
+/**
+ * Created by jaiber on 10/05/16.
+ */
+public class Util {
+
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+
+    public static final String SENDER_ID = "268651802691";
+    public static final String TOKEN = "token";
+
+    public static final String SERVER_URL = "https://gcmserver-jaibercom.c9users.io/";
+
+}
---
+++ GCM/app/src/main/java/co/edu/udea/compumovil/gcm/Util.java
@@ -0,0 +1,13 @@
+package co.edu.udea.compumovil.gcm;
+
+/**
+ * Created by jaiber on 10/05/16.
+ */
+public class Util {
+
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+
+    public static final String SENDER_ID = "277919017209";
+
+}
